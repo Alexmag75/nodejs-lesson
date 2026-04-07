39525ccb2892dbf9a5538695b92f0d6d6e3f92c7
Alexmag75 <aleksandrmargrarit@gmail.com>
2026-04-07 17:28:04 +0300

    07_04

--- index.js
+++ index.js
@@ -40,3 +40,82 @@
 // startSurvey();
 
 //lesson2
+
+// Створення файлу, чтение и вывод в консоль
+// const fs = require('node:fs/promises');
+// async function fsProm() {
+//     await fs.writeFile('test.txt', 'Hello World');
+//     const data=await fs.readFile('test.txt', 'utf-8');
+//     console.log(data);
+// }
+// void fsProm();
+
+// добавление данных в файл
+
+// const path = require('node:path');
+// const pathToFile=path.join(__dirname,'test.txt');
+// const fs = require('node:fs/promises');
+// async function fsProm() {
+//     await fs.writeFile(pathToFile, 'Hello World ');
+//     const data=await fs.readFile('test.txt', 'utf-8');
+//     console.log(data);
+//     await fs.appendFile(pathToFile,'Hello World 2');
+//     await fs.mkdir(path.join(__dirname, 'new-folder', 'new2'),{recursive:true});
+//     // await fs.rm(path.join(__dirname, 'new-folder', 'new2'),{recursive:true});
+//     // await fs.rm(path.join(__dirname, 'new-folder'),{recursive:true});
+// }
+// void fsProm();
+
+//создание директории
+
+// const fs = require('node:fs/promises');
+// const path = require('node:path');
+// async function checkAndCreateDir() {
+//     const dirPath = path.join(__dirname, 'new-folder');
+//     try {
+//         await fs.access(dirPath);
+//         console.log('Папка уже существует, ничего делать не нужно.');
+//     } catch {
+//         console.log('Папки нет. Создаю...');
+//         await fs.mkdir(dirPath);
+//         console.log('Папка успешно создана!');
+//     }
+// }
+// void checkAndCreateDir();
+
+//переименование файла и перенос в созданную папку + копирование
+
+// const path = require('node:path');
+// const pathToFile=path.join(__dirname,'test.txt');
+// const fs = require('node:fs/promises');
+// async function fsProm() {
+//     await fs.writeFile(pathToFile, 'Hello World ');
+//     await fs.readFile('test.txt', 'utf-8');
+//     await fs.mkdir(path.join(__dirname, 'new-folder'), { recursive: true });
+//     //await fs.rename(pathToFile,path.join(__dirname,'new-folder','test2.txt'));
+//     //await fs.copyFile(pathToFile,path.join(__dirname,'new-folder','test2.txt'));
+//
+//     const stat=await fs.stat(pathToFile);
+//     console.log(stat.isFile());
+// }
+// void fsProm();
+
+//работа с большими файлами
+
+const path = require('node:path');
+const pathToFile=path.join(__dirname,'8-inform-korshunova.pdf');
+const fs = require('node:fs');
+async function fsProm() {
+
+    const readStream=fs.createReadStream(pathToFile);
+    const writeStream=fs.createWriteStream(path.join(__dirname,'8-inform.pdf'));
+
+    readStream.on('data', (chunk) => {
+        console.log('chunk', chunk.length);
+        writeStream.write(chunk);
+
+    })
+       //readStream.pipe(writeStream);
+}
+void fsProm();
+
