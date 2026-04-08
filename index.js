43ada6bdd122adaa7fc8a97fdb0e5fb0980061d3
Alexmag75 <aleksandrmargrarit@gmail.com>
2026-04-08 17:21:14 +0300

    08-04

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Lesson1
+//Lesson1,2
 
 // const http = require('node:http');
 //
@@ -11,11 +11,11 @@
 // });
 //
 // server.listen(8000);
-
+//
 // const path = require('node:path');
 // const pathToFile= __filename;
 // console.log(path.dirname(pathToFile))
-
+//
 // const readline = require('node:readline/promises');
 //
 // const rlInstance = readline.createInterface({
@@ -38,9 +38,9 @@
 //     }
 // }
 // startSurvey();
-
-//lesson2
-
+//
+// lesson2
+//
 // Створення файлу, чтение и вывод в консоль
 // const fs = require('node:fs/promises');
 // async function fsProm() {
@@ -49,9 +49,9 @@
 //     console.log(data);
 // }
 // void fsProm();
-
+//
 // добавление данных в файл
-
+//
 // const path = require('node:path');
 // const pathToFile=path.join(__dirname,'test.txt');
 // const fs = require('node:fs/promises');
@@ -65,9 +65,9 @@
 //     // await fs.rm(path.join(__dirname, 'new-folder'),{recursive:true});
 // }
 // void fsProm();
-
-//создание директории
-
+//
+// создание директории
+//
 // const fs = require('node:fs/promises');
 // const path = require('node:path');
 // async function checkAndCreateDir() {
@@ -82,9 +82,9 @@
 //     }
 // }
 // void checkAndCreateDir();
-
-//переименование файла и перенос в созданную папку + копирование
-
+//
+// переименование файла и перенос в созданную папку + копирование
+//
 // const path = require('node:path');
 // const pathToFile=path.join(__dirname,'test.txt');
 // const fs = require('node:fs/promises');
@@ -99,9 +99,9 @@
 //     console.log(stat.isFile());
 // }
 // void fsProm();
-
-//работа с большими файлами
-
+//
+// работа с большими файлами
+//
 // const path = require('node:path');
 // const pathToFile=path.join(__dirname,'8-inform-korshunova.pdf');
 // const fs = require('node:fs');
@@ -118,9 +118,9 @@
 //        //readStream.pipe(writeStream);
 // }
 // void fsProm();
-
+//
 // Event
-
+//
 // const EventEmitter=require('node:events');
 // const emitter = new EventEmitter();
 //
@@ -140,9 +140,9 @@
 //
 // }
 // void fsProm();
-
-//os
-
+//
+// os
+//
 // const os=require('node:os');
 // const {exec}=require('child_process');
 // async function fsProm() {
@@ -150,3 +150,144 @@
 // }
 // void fsProm();
 
+//lesson 3
+
+// get
+// const express = require("express");
+// const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.get('/users', (req, res) => {
+//     res.send('Hello World 1');
+// })
+// app.post('/user', (req, res) => {
+//     res.send('Hello World 2');
+// })
+// app.listen(3000,()=>{
+//     console.log('listening on port 3000');
+// });
+
+//post
+
+// const express = require("express");
+// const app = express();
+//
+// app.use(express.json());
+// let users = [
+//     { id: 1, name: 'Ivan' }
+// ];
+//
+// app.post('/users', (req, res) => {
+//     const newName = req.body.name;
+//
+//     const newUser = {
+//         id: users.length + 1,
+//         name: newName
+//     };
+//     users.push(newUser);
+//     res.status(201).json({
+//         message: 'Пользователь успешно создан!',
+//         user: newUser
+//     });
+//     // console.log(req.body);
+//     // console.log(reg.query);
+//     // console.log(reg.params);
+// });
+//
+// app.get('/users', (req, res) => {
+//     res.json(users);
+// });
+// app.listen(3000, () => {
+//     console.log('Сервер запущен: http://localhost:3000');
+// });
+
+// Create, Read, Update, Delete
+
+// const express = require("express");
+//
+// const app = express();
+//
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+//
+// const users = [
+//     {id: 1, name: 'Maksym', email: 'feden@gmail.com', password: 'qwe123'},
+//     {id: 2, name: 'Alina', email: 'alindosik@gmail.com', password: 'ert345'},
+//     {id: 3, name: 'Anna', email: 'ann43@gmail.com', password: 'ghj393'},
+//     {id: 4, name: 'Tamara', email: 'tomochka23@gmail.com', password: 'afs787'},
+//     {id: 5, name: 'Dima', email: 'taper@gmail.com', password: 'rtt443'},
+//     {id: 6, name: 'Rita', email: 'torpeda@gmail.com', password: 'vcx344'},
+//     {id: 7, name: 'Denis', email: 'denchik@gmail.com', password: 'sdf555'},
+//     {id: 8, name: 'Sergey', email: 'BigBoss@gmail.com', password: 'ccc322'},
+//     {id: 9, name: 'Angela', email: 'lala@gmail.com', password: 'cdd343'},
+//     {id: 10, name: 'Irina', email: 'irka7@gmail.com', password: 'kkk222'},
+// ];
+//
+// app.get('/users', (req, res) => {
+//     try {
+//         res.send(users);
+//     } catch (e) {
+//         res.status(500).send(e.message);
+//     }
+// });
+//
+// app.post('/users', (req, res) => {
+//     try {
+//         const {name, email, password} = req.body;
+//         const id = users[users.length - 1].id + 1;
+//         const newUser = {id, name, email, password};
+//         users.push(newUser);
+//         res.status(201).send(newUser);
+//     } catch (e) {
+//         res.status(500).send(e.message);
+//     }
+// });
+//
+// app.get('/users/:userId', (req, res) => {
+//     try {
+//         const userId = Number(req.params.userId);
+//         const user = users.find(user => user.id === userId);
+//         if (!user) {
+//             return res.status(404).send('User not found');
+//         }
+//         res.send(user);
+//     } catch (e) {
+//         res.status(500).send(e.message);
+//     }
+// });
+//
+// app.put('/users/:userId', (req, res) => {
+//     try {
+//         const userId = Number(req.params.userId);
+//         const userIndex = users.findIndex(user => user.id === userId);
+//         if (userIndex === -1) {
+//             return res.status(404).send('User not found');
+//         }
+//         const {name, email, password} = req.body;
+//         //TODO validate data
+//         // users[userIndex] = {...users[userIndex], name, email, password};
+//         users[userIndex].name = name;
+//         users[userIndex].email = email;
+//         users[userIndex].password = password;
+//         res.status(201).send(users[userIndex]);
+//     } catch (e) {
+//         res.status(500).send(e.message);
+//     }
+// });
+//
+// app.delete('/users/:userId', (req, res) => {
+//     try {
+//         const userId = Number(req.params.userId);
+//         const userIndex = users.findIndex(user => user.id === userId);
+//         if (userIndex === -1) {
+//             return res.status(404).send('User not found');
+//         }
+//         users.splice(userIndex, 1);
+//         res.sendStatus(204);
+//     } catch (e) {
+//         res.status(500).send(e.message);
+//     }
+// });
+// app.listen(3000,()=>{
+//     console.log('listening on port 3000');
+// });
