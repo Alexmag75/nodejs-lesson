4e0a40335d37741407d187149bc04976256fa3f9
Alexmag75 <aleksandrmargrarit@gmail.com>
2026-04-12 08:33:42 +0300

    Lesson 12_04

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,21 +8,6 @@ class UserService {
   }
 
   public async create(dto: Partial<IUser>): Promise<IUser> {
-    if (!dto.name || dto.name.length < 3) {
-      throw new ApiError("Имя должно быть длиннее 3 символов", 400);
-    }
-    if (!dto.email || !dto.email.includes("@")) {
-      throw new ApiError(
-        "Адрес электронной почты обязателен и должен существовать",
-        400,
-      );
-    }
-    if (!dto.password || dto.password.length < 6) {
-      throw new ApiError(
-        "Длина пароля должна составлять не менее 6 символов.",
-        400,
-      );
-    }
     return await userRepository.create(dto);
   }
 
@@ -35,12 +20,6 @@ class UserService {
   }
 
   public async updateById(userId: string, dto: Partial<IUser>): Promise<IUser> {
-    if (dto.name && dto.name.length < 3) {
-      throw new ApiError("Новое имя слишком короткое", 400);
-    }
-    if (dto.email && !dto.email.includes("@")) {
-      throw new ApiError("Некорректный формат email", 400);
-    }
     const updatedUser = await userRepository.updateById(userId, dto as IUser);
     if (!updatedUser) {
       throw new ApiError("Пользователь не найден", 404);
